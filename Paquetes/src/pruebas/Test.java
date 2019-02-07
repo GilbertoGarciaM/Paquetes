b21a847deb565939256bd0aeb03823e1f3fb0dc0
Alumno <Alumno@ws204-12>
2019-02-06 18:00:03 -0800

    primera version

---
+++ Paquetes/src/pruebas/Test.java
@@ -0,0 +1,19 @@
+package pruebas;
+
+public class Test {
+
+	public static void main(String[] args){
+		
+		
+	paquete1.Persona p= new paquete1.Persona(23,"Maria");
+	paquete2.persona n= new paquete2.persona(25,"Gilberto","Masculino");
+	System.out.println(p.toString());
+	System.out.println(n.toString());
+	
+		
+	}
+	
+	
+	
+	
+}
