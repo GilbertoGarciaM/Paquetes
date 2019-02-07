eb54a5baba588650b538de3d097de7aab870867e
Gilberto Garcia Marquez <42616390+GilbertoGarciaM@users.noreply.github.com>
2019-02-06 18:08:57 -0800

    cambios de prueba

--- Paquetes/src/pruebas/Test.java
+++ Paquetes/src/pruebas/Test.java
@@ -7,9 +7,10 @@ public class Test {
 		
 	paquete1.Persona p= new paquete1.Persona(23,"Maria");
 	paquete2.persona n= new paquete2.persona(25,"Gilberto","Masculino");
+	System.out.println("inicio de impresion:");	
 	System.out.println(p.toString());
 	System.out.println(n.toString());
-	
+	System.out.println("FIN");
 		
 	}
 	
