4af79b7ebb00eddd467fe2f76b4232bde15c0a88
Alumno <Alumno@ws204-12>
2019-02-06 18:12:17 -0800

    cambio local

--- Paquetes/src/pruebas/Test.java
+++ Paquetes/src/pruebas/Test.java
@@ -7,8 +7,10 @@ public class Test {
 		
 	paquete1.Persona p= new paquete1.Persona(23,"Maria");
 	paquete2.persona n= new paquete2.persona(25,"Gilberto","Masculino");
+	paquete1.Persona s=new paquete1.Persona(24, "Fulano");
 	System.out.println(p.toString());
 	System.out.println(n.toString());
+	System.out.println(s.toString());
 	
 		
 	}
